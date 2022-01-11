3383f89368c90dc22cda19aa30b0050849621c91
vagrant <mustafakya88@hotmail.com>
2022-01-11 02:28:36 +0000

    created homepage, register routes and register.ejs

--- server.js
+++ server.js
@@ -38,10 +38,16 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 
+const homepageRoute = require("./routes/homepage-route");
+const registerRoute = require("./routes/register-route");
+
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+
+app.use("/homepage", homepageRoute(db));
+app.use("/register", registerRoute(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
