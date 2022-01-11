96868167939f3c65ad2c0b099c5289e99bde0fe3
MustafaSKaya <mustafakya88@hotmail.com>
2022-01-11 23:49:59 +0000

    new routes

--- routes/homepage-route.js
+++ routes/homepage-route.js
@@ -1,4 +1,4 @@
-const { request } = require('express');
+const { request, response } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -8,7 +8,7 @@ const homepageRoute = (db) => {
         response.render("index")
     });
 
-    router.get("/profile", (request, response) => {
+    router.get("/tasks", (request, response) => {
         db.query('SELECT * FROM tasks;')
           .then((task) => {
             const templateVars = { tasks : task.rows }
@@ -16,50 +16,60 @@ const homepageRoute = (db) => {
         })
     });
 
-    router.get("/profile/eat", (request, response) => {
+    router.get("/tasks/eat", (request, response) => {
         db.query('SELECT * FROM tasks JOIN todolists ON tasks.id = task_id WHERE category_id = 2;')
-          .then((watchlist) => {
-            response.send(watchlist.rows);
+          .then((toEat) => {
+            const templateVars = { toEats : toEat.rows }
+            response.render("eatList", templateVars);
         });
     });
 
-    router.get("/profile/read", (request, response) => {
+    router.get("/tasks/read", (request, response) => {
         db.query('SELECT * FROM tasks JOIN todolists ON tasks.id = task_id WHERE category_id = 3;')
-          .then((watchlist) => {
-            response.send(watchlist.rows);
+          .then((toRead) => {
+            const templateVars = { toReads : toRead.rows }
+            response.render("readList", templateVars);
         });
     });
 
-    router.get("/profile/buy", (request, response) => {
+    router.get("/tasks/buy", (request, response) => {
         db.query('SELECT * FROM tasks JOIN todolists ON tasks.id = task_id WHERE category_id = 4;')
-          .then((watchlist) => {
-            response.send(watchlist.rows);
+          .then((toBuy) => {
+            const templateVars = { toBuys : toBuy.rows }
+            response.render("buyList", templateVars);
         });
     });
 
-    router.get("/profile/watch", (request, response) => {
+    router.get("/tasks/watch", (request, response) => {
         db.query('SELECT * FROM tasks JOIN todolists ON tasks.id = task_id WHERE category_id = 1;')
-          .then((watchlist) => {
-            response.send(watchlist.rows);
+          .then((toWatch) => {
+            const templateVars = { toWatchs : toWatch.rows }
+            response.render("watchList", templateVars);
         });
     });
 
-    router.get("/profile/completed", (request, response) => {
-        db.query('SELECT * FROM tasks JOIN todolists ON tasks.id = task_id WHERE status_name = "Not completed";')
-          .then((watchlist) => {
-            response.send(watchlist.rows);
+    router.get("/tasks/completed", (request, response) => {
+        db.query(`SELECT * FROM tasks JOIN statuses ON statuses.id = status_id WHERE status_name = 'Completed';`)
+          .then((completed) => {
+            const templateVars = { completeds : completed.rows }
+            response.render("completed", templateVars);
         });
     });
 
-    router.get("/profile/archived", (request, response) => {
-        db.query('SELECT * FROM tasks WHERE archived = "f";')
-          .then((watchlist) => {
-            response.send(watchlist.rows);
+    router.get("/tasks/archived", (request, response) => {
+        db.query('SELECT * FROM tasks WHERE archived IS NULL;')
+          .then((archived) => {
+            const templateVars = { archiveds : archived.rows }
+            response.render("archived", templateVars);
         });
     });
 
-    router.get("/profile/newtask", (request, response) => {
+    router.get("/tasks/newtask", (request, response) => {
         response.render("id");
+    });
+
+    router.get("/tasks/profile", (request, response) => {
+        response.render("profile");
     })
 
     return router;
