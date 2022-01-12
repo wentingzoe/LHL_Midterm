de56f039d6039f5e5a8adfd95b796e1510d65724
wentingzoe <ywtzoe@gmail.com>
2022-01-11 20:24:25 -0500

    create addtask.js to link local db, update sidebar.ejs

--- public/scripts/addtask.js
+++ public/scripts/addtask.js
@@ -1,103 +1,27 @@
-// Test Script To Print User Names
+// lINK THE INPUT DATA TO LOCAL DATABASE
 
-// 1. Function to create a single task element
-const createTaskElement = function(data) {
+const { Pool } = require('pg');
 
-  const $output = $('<article class="tasks lined thick">'); // Output is a tasks class article
-
-  // Checkbox
-  const $checkbox = $(`<button class="checkbox" onClick="checkTask(this, ${data.id})"></button>`);
-
-  // Task Content Container
-  const $taskContent = $('<div class="taskContent">');
-
-  // Task Header Tags
-  const $taskHeader = $('<header class="taskHeader">'); // Set task header tag
-  const $taskTitle = $('<p class="taskTitle">').text(data.task_title); // Set p tag in task header
-  // Set edit button for task title
-  const $deleteTask = $(`<button class="deleteTask btn btn-danger" onClick="deleteTask(${data.id})">`).text('❌')
-
-  // Append Header Tags
-  // $taskHeader.append($taskTitle).append($editTitle).append($deleteTask);
-  $taskHeader.append($taskTitle).append($deleteTask);
-
-  // Task Body Tags
-  const $taskBody = $('<div class="taskBody">'); // Set div with task body class
-  const $taskDescription = $('<p class="taskDescription">').text(data.task_description); // Set p with taskDescription class
-  const $editButtons = $('<div class="editButtons">') // Set div with editButtons class
-  const $editButton = $(`<button class="btn btn-outline-secondary" onClick="editTask(this, ${data.id})">`).html('<i class="far fa-edit"></i>');
-
-  // Append Body Tags
-  $editButtons.append($editButton);
-  $taskBody.append($taskDescription).append($editButtons);
-
-  // Task Footer
-  const $taskFooter = $('<footer class="taskFooter">'); // Set footer tag with taskFooter class
-
-
-    const $categoryContainer = $('<div class="categoryContainer">'); // Set div with class categoryContainer
-
-    let currentCat = false;
-    if(data.category_name) {
-      // For the real script, there should be a loop here for each category including the append step!
-      const $removeCategories = $(`<button class="removeCategories thin line ${data.category_name}" id="currentCategory" onClick="deleteCategoryFromTask(${data.id},${data.category_id})">`).text(`❌ ${data.category_name}`);
-      $categoryContainer.append($removeCategories);
-      currentCat = true;
-    }
-    const $filmsCategory = $(`<button class="addCategoryButton Films" id="filmsCategory${data.id}" onClick="addCategoryToTask(${data.id},1)">`).text('➕\nFilms');
-    const $seriesCategory = $(`<button class="addCategoryButton TV Series" id="seriesCategory${data.id}" onClick="addCategoryToTask(${data.id},2)">`).text('➕\nSeries');
-    const $restaurantCategory = $(`<button class="addCategoryButton Restaurants and Cafes" id="restaurantCategory${data.id}" onClick="addCategoryToTask(${data.id},3)">`).text('➕\nResturants');
-    const $booksCategory = $(`<button class="addCategoryButton Books" id="booksCategory${data.id}" onClick="addCategoryToTask(${data.id},4)">`).text('➕\nBooks');
-    const $shoppingCategory = $(`<button class="addCategoryButton Shopping" id="shoppingCategory${data.id}" onClick="addCategoryToTask(${data.id},5)">`).text('➕\nShopping');
-    $categoryContainer.append($filmsCategory).append($seriesCategory).append($restaurantCategory).append($booksCategory).append($shoppingCategory);
-
-    let $addCategory = $(`<button class="btn btn-outline-secondary" id="addCategory${data.id}" onClick="showCategories(${data.id})" style="border: none;">`).html('<i class="fas fa-plus-square"></i>'); // Set addCategory button
-    if(currentCat) {
-      $addCategory = $(`<button class="btn btn-outline-secondary" id="addCategory${data.id}" onClick="showCategories(${data.id})" style="border: none; display:none;">`).html('<i class="fas fa-plus-square"></i>'); // Set addCategory button
-    }
-    // Append Footer tags
-    $taskFooter.append($categoryContainer).append($addCategory);
-
-  // Append $output
-  $taskContent.append($taskHeader).append($taskBody).append($taskFooter);
-  $output.append($checkbox).append($taskContent);
-
-  return $output;
-};
-
-// 2. Function to loop through example data set and render all tasks
-const renderTasks = function(data) {
-  $('#allTasks').html(''); // Clears default text
-
-  // Code for custom number of columns
-  const columns = 3; // Change this number to adjust number of columns.
-  const rows = Math.ceil(data.length/columns);
-  let counter = 0; // Needed to access JSON data.
-
-  for (let i = 0; i < rows; i++) {
-    let $row = $('<div class="row">');
-    for (let j = 0; j < columns; j++) {
-      let $column = $('<div class="column">');
-      if (data[counter] && data[counter].status_id === 1) {
-        $task = createTaskElement(data[counter]); // calls createTaskElement for each task
-        $column.prepend($task); // takes return value and prepends (ensures order) it to the tasks container
-      }
-      $row.append($column);
-      $('#allTasks').append($row);
-      counter ++;
-    }
-  }
-};
-
-// 3. Create a function to do this directly from the database API
-const loadTasks = function() {
-  $.getJSON('/api/tasks', function(data) {
-    // jQuery shorthand for Ajax
-    renderTasks(data.tasks);
-  });
-};
-
-$(document).ready(function() {
-  // 4. Call load function
-  loadTasks()
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm'
 });
+/**
+ * Add a new task to the database.
+ * @param {{task_title: string , task_description: string}} task
+ * @return {Promise<{}>} A promise to the task.
+ */
+ const addTask =  function(task) {
+
+  const queryString = `INSERT INTO tasks (user_id, status_id, task_title, task_description,start_date, end_date, archived) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *`;
+  const values = [task.user_id, task.status_id, task.task_title, task.task_description, task.start_date, task.end_date, task.archived];
+  return pool
+  .query(queryString,values)
+  .then(res => res.rows)
+  .catch(err => console.error(err.message));
+}
+exports.addtask = addtask;
+
+
--- routes/tasks.js
+++ routes/tasks.js
@@ -7,11 +7,11 @@ module.exports = (db) => {
     console.log(req.body)
 
       let queryString = `
-        INSERT INTO tasks (task_title, task_description, user_id, status_id, start_date,end_date)
-        VALUES ($1, $2, $3, $4, $5, $6)
+        INSERT INTO tasks (task_title, task_description, user_id, status_id)
+        VALUES ($1, $2, $3, $4)
         RETURNING *;
       `;
-      let queryParams = [req.body.task_title, req.body.task_desc, 1, 1,req.body.start_date, req.body.end_date];
+      let queryParams = [req.body.task_title, req.body.task_desc, 1, 1];
       db.query(queryString, queryParams)
       .then((data) => {
         const tasks = data.rows;
