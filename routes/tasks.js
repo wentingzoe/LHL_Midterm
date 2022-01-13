3dd6d742214cacead06d04383854c77d20466448
vagrant <rah.leak@gmail.com>
2022-01-13 17:58:03 +0000

    commit to switch

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-// const {checkProduct} = require('../categories/buyapi')
+const {checkFood} = require('../categories/eatapi')
 
 
 module.exports = (db) => {
@@ -8,28 +8,28 @@ module.exports = (db) => {
   router.post("/add", (req, res) => {
     console.log("test!")
     console.log(req.body)
-    // checkProduct(req.body.task_title);
-      let queryString = `
-        INSERT INTO tasks (task_title, task_description, user_id, category_id)
-        VALUES ($1, $2, $3, $4)
-        RETURNING *;
-      `;
+    checkFood(req.body.task_title);
+      // let queryString = `
+      //   INSERT INTO tasks (task_title, task_description, user_id, category_id)
+      //   VALUES ($1, $2, $3, $4)
+      //   RETURNING *;
+      // `;
 
-      let queryParams = [req.body.task_title, req.body.task_description, 1, 1];
-      db.query(queryString, queryParams)
-      .then((data) => {
+      // let queryParams = [req.body.task_title, req.body.task_description, 1, 1];
+      // db.query(queryString, queryParams)
+      // .then((data) => {
 
-        const tasks = data.rows;
-        console.log("Successfully, inserted!");
-        res.json({ tasks });
-      })
-      .catch(err => {
-        console.log("I suck", err);
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-      res.redirect("/tasks");
+      //   const tasks = data.rows;
+      //   console.log("Successfully, inserted!");
+      //   res.json({ tasks });
+      // })
+      // .catch(err => {
+      //   console.log("I suck", err);
+      //   res
+      //     .status(500)
+      //     .json({ error: err.message });
+      // });
+      // res.redirect("/tasks");
 
   });
 
